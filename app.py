415f8d4538fec948a260138907fae3549a35fe2c
Shamikh98 <92282274+Shamikh98@users.noreply.github.com>
2025-04-24 16:48:31 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -5,15 +5,21 @@ import plotly.express as px
 
 df_cars= pd.read_csv('vehicles_us.csv')
 
-agree = st.checkbox("Check if interested in cars less than 20000 miles")
+agree1 = st.checkbox("Check if interested in cars less than 20000 miles")
 
-if agree:
+if agree1:
     df_cars = df_cars[df_cars['odometer'] <= 20000]
     
 
-st.plotly_chart(px.histogram(df_cars, x='price', nbins=50, title='Car Prices'))
+st.plotly_chart(px.scatter(df_cars, x='odometer', y='price', title='Car Prices vs Odometer'))
 
+agree2 = st.checkbox("Check if year >= 2005")
 
+if agree2:
+    df_cars = df_cars[df_cars['model_year'] >= 2011]
+    
+
+st.plotly_chart(px.histogram(df_cars, x='model_year', nbins=50, title='Car Prices'))
 
 st.header('Car Analysis')
 
